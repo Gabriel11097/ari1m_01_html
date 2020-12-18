1fecddaceff7c0a0ad3d493e3cb3038b878aae85
Gabriel11097 <gabidf110@gmail.com>
2020-12-18 13:37:36 +0100

    practica 4

--- P3escalas.dwl
+++
@@ -1,3 +0,0 @@
-gabid
-LAPTOP-2NE924G4 
-miércoles, 2 de diciembre de 2020  12:26:14
---
+++ programacion.php
@@ -0,0 +1,26 @@
+<h1>
+Ley de Ohm
+</h1>
+<form>
+V<input placeholder="voltaje" value="20"  name ="v" id="v" onkeyup="calcular()"/>
+R<input placeholder="resistencia" value="1" name ="r" id="r" onkeyup="calcular()"/>
+<button onclick="calcular()"> Calcular</button>
+</form>
+<div id="i" style="padding:20px; background-color:pink"> Resultado</div>
+<script>
+//alert("hola mundo");
+//console.log("hola otra vez");
+function calcular(){
+v=document.getElementById("v").value;
+r=document.getElementById("r").value;
+i=v/r;
+document.getElementById("i").innerHTML= i+" Amperios";
+}
+calcular();
+</script>
+
+<?php
+
+if(isset($_GET))print_r($_GET);
+
+?>
