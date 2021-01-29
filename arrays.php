fd31abb52d0b866ee427bf9c22f4694664b83228
Gabriel11097 <gabidf110@gmail.com>
2021-01-29 15:56:35 +0100

    urbanizacion

---
+++ arrays.php
@@ -0,0 +1,21 @@
+<?php
+$resultados = [
+	'0',
+	'6',
+	'12',
+	'18',
+	'24',
+	'30',
+	'36',
+	'42',
+	'48',
+	'54',
+	'60',
+];
+//Tabla del 6
+echo "Tabla del 6:<br>";
+foreach ($resultados as $c){
+	
+		echo "$c <br>";
+	
+}
